b8ecf1c34e91fd04d7aa0abc62c1fb91d0d538e9
Sujit Poojari <sujitpoojari@hotmail.com>
2026-01-14 01:25:21 +0530

    Initial commit

---
+++ src/main/java/com/sujit/devops_portfolio/SujitDevopsPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.sujit.devops_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SujitDevopsPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SujitDevopsPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujit/devops_portfolio/SujitDevopsPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujit.devops_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SujitDevopsPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
